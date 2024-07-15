d28532ea70eda910fd7978341a9857085efe36c1
nds-777 <133801496+nikunj-sorathiya@users.noreply.github.com>
2024-07-15 22:12:22 +0530

    Upgrade Version-1.2

--- nds.sh
+++ nds.sh
@@ -9,13 +9,27 @@ echo " | | | || (_| |\__ \ "
 echo " |_| |_| \__,_||___/ "
 echo "                     "
 
-
 # Color codes
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 RED='\033[1;31m'
 BLINK='\033[5m'
 RESET='\033[0m'
+
+# Function to check if script is globally set
+check_global() {
+  if [[ ":$PATH:" == *":/usr/local/bin:"* ]]; then
+    if [ -f "/usr/local/bin/nds" ]; then
+      return 0
+    fi
+  fi
+
+  echo -e "${RED}Script is not globally accessible. Setting it globally now...${RESET}"
+  sudo cp "$0" /usr/local/bin/nds
+  sudo chmod +x /usr/local/bin/nds
+  echo -e "${YELLOW}Script is now globally accessible as 'nds'.${RESET}"
+}
+check_global
 
 # Function to display PHP, NVM, Node, and NPM versions
 view_versions() {
@@ -59,7 +73,22 @@ view_versions() {
 install_nvm() {
   echo -e "${YELLOW}Installing NVM...${RESET}"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-  echo -e "${YELLOW}NVM installed successfully. Please restart your terminal or run 'source ~/.bashrc' to start using NVM.${RESET}"
+
+  echo -e "${YELLOW}Setting up NVM globally...${RESET}"
+  mkdir -p /usr/local/nvm
+  tee /usr/local/bin/nvm.sh << 'EOF'
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+EOF
+
+  tee /etc/profile.d/nvm.sh << 'EOF'
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+EOF
+
+  ln -sf /usr/local/nvm/nvm.sh /usr/local/bin/nvm
+
+  echo -e "${YELLOW}NVM installed successfully. Please restart your terminal or run 'source /etc/profile.d/nvm.sh' to start using NVM.${RESET}"
 }
 
 # Function to install Node LTS
@@ -102,9 +131,9 @@ remove_node() {
   fi
 }
 
-# Function to remove NVM 
-remove_nvm_() {
-  echo -e "${YELLOW}Removing NVM ...${RESET}"
+# Function to remove NVM
+remove_nvm() {
+  echo -e "${YELLOW}Removing NVM...${RESET}"
   
   # Remove the NVM directory
   rm -rf "$HOME/.nvm"
@@ -115,55 +144,185 @@ remove_nvm_() {
   echo -e "${YELLOW}NVM removed. Please restart your terminal or source your profile files to apply changes.${RESET}"
 }
 
-# Main menu function
-main_menu() {
+# Function to change PHP version
+change_php_version() {
+  echo -e "${YELLOW}Changing PHP version...${RESET}"
+  sudo update-alternatives --config php
+  sudo systemctl restart apache2
+}
+
+# Function to install PHP version
+install_php_version() {
+  version=$1
+  echo -e "${YELLOW}Installing PHP ${version}...${RESET}"
+  sudo apt update
+  sudo add-apt-repository ppa:ondrej/php -y
+  sudo apt install -y php${version} php${version}-cli php${version}-fpm php${version}-mysql php${version}-curl php${version}-gd php${version}-mbstring php${version}-xml php${version}-zip libapache2-mod-php${version}
+  sudo apt update
+  echo -e "${YELLOW}PHP ${version} installed successfully.${RESET}"
+}
+
+# Function to remove PHP version
+remove_php_version() {
+  version=$1
+  echo -e "${YELLOW}Removing PHP ${version}...${RESET}"
+  sudo apt remove --purge -y php${version} php${version}-cli php${version}-fpm php${version}-mysql php${version}-curl php${version}-gd php${version}-mbstring php${version}-xml php${version}-zip libapache2-mod-php${version}
+  sudo apt autoremove -y
+  sudo apt autoclean
+  echo -e "${YELLOW}PHP ${version} removed successfully.${RESET}"
+}
+
+# Function to install Composer
+install_composer() {
+  echo -e "${YELLOW}Installing Composer...${RESET}"
+  EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+  if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
+      >&2 echo 'ERROR: Invalid installer signature'
+      rm composer-setup.php
+      exit 1
+  fi
+
+  php composer-setup.php --quiet
+  RESULT=$?
+  rm composer-setup.php
+  sudo mv composer.phar /usr/local/bin/composer
+  echo -e "${YELLOW}Composer installed successfully.${RESET}"
+  return $RESULT
+}
+
+# Function to remove Composer
+remove_composer() {
+  echo -e "${YELLOW}Removing Composer...${RESET}"
+  sudo rm /usr/local/bin/composer
+  echo -e "${YELLOW}Composer removed successfully.${RESET}"
+}
+
+# Function to show PHP menu
+php_menu() {
   while true; do
-    echo -e "${BLINK}${YELLOW}Select an option:${RESET}"
-    echo -e "${YELLOW}1. View PHP, NVM, Node, and NPM versions${RESET}"
-    echo -e "${YELLOW}2. Install NVM${RESET}"
-    echo -e "${YELLOW}3. Install Node LTS${RESET}"
-    echo -e "${YELLOW}4. Remove Node.js${RESET}"
-    echo -e "${YELLOW}5. Remove NVM ${RESET}"
-    echo -e "${YELLOW}6. Exit${RESET}"
-    read -p "Enter your choice [1-6]: " choice
+    echo -e "${BLUE} ===========|  PHP Menu  |=========== ${RESET}"
+    echo -e "${YELLOW}1. Change PHP version${RESET}"
+    echo -e "${YELLOW}2. Install PHP version${RESET}"
+    echo -e "${YELLOW}3. Remove PHP version${RESET}"
+    echo -e "${YELLOW}0. Back to main menu${RESET}"
+    read -p "Enter your choice [0-3]: " choice
 
     case $choice in
       1)
-        view_versions
+        change_php_version
         ;;
       2)
-        install_nvm
+        echo -e "${YELLOW}Select PHP version to install: 8.1, 8.2, 8.3${RESET}"
+        read -p "Enter PHP version (e.g., 8.1): " version
+        install_php_version $version
         ;;
       3)
-        install_node_lts
+        echo -e "${YELLOW}Select PHP version to remove: 8.1, 8.2, 8.3${RESET}"
+        read -p "Enter PHP version (e.g., 8.1): " version
+        remove_php_version $version
         ;;
-      4)
-        remove_node
-        ;;
-      5)
-        remove_nvm_
-        ;;
-      6)
-        echo -e "${YELLOW}Exiting...${RESET}"
-        exit 0
+      0)
+        break
         ;;
       *)
         echo -e "${RED}Invalid option selected.${RESET}"
         ;;
     esac
-
-    # Ask user whether to exit or return to menu
-    echo ""
-    read -p "Task completed. Do you want to exit? (y/n): " exit_choice
-    if [[ $exit_choice == "y" || $exit_choice == "Y" ]]; then
-      echo -e "${YELLOW}Exiting...${RESET}"
-      exit 0
-    else
-      echo -e "${YELLOW}Returning to main menu...${RESET}"
-      echo ""
-    fi
   done
 }
 
-# Start the main menu
-main_menu
+# Function to show Node menu
+node_menu() {
+  while true; do
+    echo -e "${BLUE} ===========|  Node Menu  |=========== ${RESET}"
+    echo -e "${YELLOW}1. Install Node LTS${RESET}"
+    echo -e "${YELLOW}2. Remove Node.js${RESET}"
+    echo -e "${YELLOW}3. Install NVM${RESET}"
+    echo -e "${YELLOW}4. Remove NVM${RESET}"
+    echo -e "${YELLOW}0. Back to main menu${RESET}"
+    read -p "Enter your choice [0-4]: " choice
+
+    case $choice in
+      1)
+        install_node_lts
+        ;;
+      2)
+        remove_node
+        ;;
+      3)
+        install_nvm
+        ;;
+      4)
+        remove_nvm
+        ;;
+      0)
+        break
+        ;;
+      *)
+        echo -e "${RED}Invalid option selected.${RESET}"
+        ;;
+    esac
+  done
+}
+
+# Function to show Composer menu
+composer_menu() {
+  while true; do
+    echo -e "${BLUE} ===========|  Composer Menu  |=========== ${RESET}"
+    echo -e "${YELLOW}1. Install Composer${RESET}"
+    echo -e "${YELLOW}2. Remove Composer${RESET}"
+    echo -e "${YELLOW}0. Back to main menu${RESET}"
+    read -p "Enter your choice [0-2]: " choice
+
+    case $choice in
+      1)
+        install_composer
+        ;;
+      2)
+        remove_composer
+        ;;
+      0)
+        break
+        ;;
+      *)
+        echo -e "${RED}Invalid option selected.${RESET}"
+        ;;
+    esac
+  done
+}
+
+# Main menu
+while true; do
+  echo -e "${BLUE} ===========|  Main Menu  |=========== ${RESET}"
+  echo -e "${YELLOW}1. PHP Menu${RESET}"
+  echo -e "${YELLOW}2. Node Menu${RESET}"
+  echo -e "${YELLOW}3. Composer Menu${RESET}"
+  echo -e "${YELLOW}4. Show all versions${RESET}"
+  echo -e "${YELLOW}0. Exit${RESET}"
+  read -p "Enter your choice [0-4]: " choice
+
+  case $choice in
+    1)
+      php_menu
+      ;;
+    2)
+      node_menu
+      ;;
+    3)
+      composer_menu
+      ;;
+    4)
+      view_versions
+      ;;
+    0)
+      echo -e "${YELLOW}Exiting...${RESET}"
+      exit 0
+      ;;
+    *)
+      echo -e "${RED}Invalid option selected.${RESET}"
+      ;;
+  esac
+done
